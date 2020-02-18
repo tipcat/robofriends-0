ed676c5541aa189ab30c36a489a2d6a746d0025b
David Pekerow <tipcatdp@gmail.com>
2020-02-18 01:19:36 -0500

    Initial commit

---
+++ src/robots.js
@@ -0,0 +1,92 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'John Oliver',
+    username: 'lastweektonight',
+    email: 'nothing@nowhere.com'
+  },
+  {
+    id: 2,
+    name: 'Candice Comisi',
+    username: 'cc427',
+    email: 'tacofeelings@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'David Pekerow',
+    username: 'dp710',
+    email: 'tipcatdp@gmail.com'
+  },
+  {
+    id: 4,
+    name: 'Patricia Lebsack',
+    username: 'Karianne',
+    email: 'Julianne.OConner@kory.org'
+  },
+  {
+    id: 5,
+    name: 'Chelsey Dietrich',
+    username: 'Kamren',
+    email: 'Lucio_Hettinger@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'Mrs. Dennis Schulist',
+    username: 'Leopoldo_Corkery',
+    email: 'Karley_Dach@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'Kurtis Weissnat',
+    username: 'Elwyn.Skiles',
+    email: 'Telly.Hoeger@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Nicholas Runolfsdottir V',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Glenna Reichert',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@gmail.com'
+  },
+  {
+    id: 10,
+    name: 'Clementina DuBuque',
+    username: 'Moriah.Stanton',
+    email: 'Rey.Padberg@karina.biz'
+  },
+  {
+    id: 11,
+    name: 'Seija Kapanen',
+    username: 'Seija.Kapanen',
+    email: 'SeijaKapanen@rhyta.com'
+  },
+  {
+    id: 12,
+    name: 'Kurtis Weissna',
+    username: 'Kurtis.Weissna',
+    email: 'ArsiPaavolainen@rhyta.com'
+  },
+  {
+    id: 13,
+    name: 'Emilie M. Eriksen',
+    username: 'eeriksen11',
+    email: 'EmilieMEriksen@jourrapide.com'
+  },
+  {
+    id: 14,
+    name: 'Meikashishikuto',
+    username: 'Meika999',
+    email: 'Meikashishikuto@rhyta.com'
+  },
+  {
+    id: 15,
+    name: 'Petrine Knudsen',
+    username: 'Petrine.Knudsen',
+    email: 'PetrineKnudsen@armyspy.com'
+  }
+];

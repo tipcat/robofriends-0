403f5a27b5e3f7c1267486d63395ebfb56982627
David Pekerow <tipcatdp@gmail.com>
2020-02-20 23:35:24 -0500

    added app functionality

--- src/CardList.js
+++ src/CardList.js
@@ -1,13 +1,40 @@
 import React from 'react';
 import Card from './Card';
-
+/*
 const CardList = ({ robots }) => {
     const cardComponent = robots.map((user, i)  => {
-        return <Card id={robots[i].id} name={robots[i].name} email={robots[i].email}/>
+        return (
+            <Card 
+                key={i} 
+                id={robots[i].id} 
+                name={robots[i].name} 
+                email={robots[i].email}
+            />
+        );
     })
     return (
         <div>
             { cardComponent }
+        </div>
+    );
+}
+*/
+
+const CardList = ({ robots }) => {
+    return (
+        <div>
+            {
+                robots.map((user, i) => {
+                    return (
+                        <Card
+                            key = {i}
+                            id = {robots[i].id}
+                            name = {robots[i].name}
+                            email = {robots[i].email}
+                        />
+                    );
+                })
+            }
         </div>
     );
 }
